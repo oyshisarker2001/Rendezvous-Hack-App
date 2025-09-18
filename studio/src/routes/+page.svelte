de38fd1bfb2cdb687ea1d5038196caae7148bdfb
Oyshi Pronomy Sarker <01oyshipronomy@gmail.com>
2025-09-17 23:03:49 -0700

    studio

---
+++ studio/src/routes/+page.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+	import * as Breadcrumb from '$lib/components/ui/breadcrumb';
+	import * as Sidebar from '$lib/components/ui/sidebar';
+
+	import { Separator } from '$lib/components/ui/separator';
+
+	import AppSidebar from '$lib/components/app-sidebar.svelte';
+</script>
+
+<Sidebar.Provider>
+	<AppSidebar />
+	<Sidebar.Inset>
+		<header class="bg-background sticky top-0 flex h-16 shrink-0 items-center gap-2 border-b px-4">
+			<Sidebar.Trigger class="-ml-1" />
+			<Separator orientation="vertical" class="mr-2 h-4" />
+			<Breadcrumb.Root class="flex-grow">
+				<Breadcrumb.List>
+					<Breadcrumb.Item class="hidden md:block">
+						<Breadcrumb.Link>Home</Breadcrumb.Link>
+					</Breadcrumb.Item>
+				</Breadcrumb.List>
+			</Breadcrumb.Root>
+		</header>
+	</Sidebar.Inset>
+</Sidebar.Provider>
